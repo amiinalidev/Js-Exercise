38d242c3d650a5920832311189299060e720f114
amiinalidev <amiinaliidev@gmail.com>
2026-06-03 21:25:02 -0700

    add js exercise 4 file

---
+++ js exercise 4/function.js
@@ -0,0 +1,15 @@
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(2,3));
+// console.log(add(10,-4));
+
+
+
+// function expression
+
+const add=function(a,b){
+    return a+b;
+}
+console.log(add(2,3));
+console.log(add(4,5));
