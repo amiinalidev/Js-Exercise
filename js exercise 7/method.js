7ae3f4d1db483c4590be0c718514915217fed9a1
amiinalidev <amiinaliidev@gmail.com>
2026-07-02 14:34:01 -0700

    added js exercise 7

---
+++ js exercise 7/method.js
@@ -0,0 +1,12 @@
+let colors = [ "red", "blue", "green"];
+console.log(colors);
+
+console.log("after push");
+
+colors.push("yellow");
+console.log(colors);
+
+console.log(" after shift");
+
+colors.shift();
+console.log(colors);
