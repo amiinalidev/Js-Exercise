0c22f3867e479150b148afcd779bc535838a3e0d
amiinalidev <amiinaliidev@gmail.com>
2026-07-13 13:09:33 -0700

    added js exercise 9

---
+++ js exercise 9/apps.js
@@ -0,0 +1,14 @@
+let car = {
+    make : "tayota",
+    year : 2020,
+    model : "camry",
+
+    start: function() {
+        console.log("the car has been started");
+    }
+
+}
+car.start();
+console.log(car.make);
+console.log(car.year);
+console.log(car.model);
