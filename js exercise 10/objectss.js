1062250e26b42b0b1debd94be23e19d1ab5c4538
amiinalidev <amiinaliidev@gmail.com>
2026-07-16 13:07:58 -0700

    added all js exercise 10

---
+++ js exercise 10/objectss.js
@@ -0,0 +1,15 @@
+let libarayBooks = [ "afsomali", "geography"];
+let properties =[
+    {
+        title: "gabey",
+        author: "ahmed",
+        yer: 2020
+    },
+    {
+        title: "Othman",
+        author: "ali",
+        yer: 2018
+    }
+]
+console.log(properties[0].title);
+console.log(properties[1].author);
