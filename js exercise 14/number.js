7a76db2c7aca43ef71b897abd5fcbdf4d27f7159
amiinalidev <amiinaliidev@gmail.com>
2026-08-04 01:28:33 +0200

    added js exercise 14 and the files

---
+++ js exercise 14/number.js
@@ -0,0 +1,11 @@
+let number;
+do{
+    number=prompt("enter a number that greater than 10");
+}while(number<10);
+if(number>10){
+    console.log(
+        "the number is greater than 10"
+    )
+   
+}
+ console.log("welconme user");
