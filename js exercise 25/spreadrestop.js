a6011052d3a381bff4372a96444417c50fe93e89
amiinalidev <amiinaliidev@gmail.com>
2026-09-23 23:53:20 +0200

    added all file puls pics

---
+++ js exercise 25/spreadrestop.js
@@ -0,0 +1,10 @@
+const numbers =[1,2,3];
+const newNumbers =[...numbers, 4,5,6];
+console.log(newNumbers);
+
+
+// rest operator
+function multiply(...numbers){
+    return numbers.reduce((total, number)=> total * number);
+}
+console.log(multiply(2,4));
